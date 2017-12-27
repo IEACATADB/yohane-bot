9351f1b3873ada1f1979514c75400d251a503cf1
IEACATADB <eugenehope94@gmail.com>
2017-12-27 20:30:35 +0300

    made server link as a command instead of outputting it to console
    also backing up before checking out CommandBot

--- main.rb
+++ main.rb
@@ -2,17 +2,31 @@ require 'discordrb'
 
 require './modules/main.rb'
 
+prefix = '†'
 
 bot = Discordrb::Bot.new token: ENV['bot_token'], client_id: ENV['client_id']
 
-
-puts "This bot's invite URL is #{bot.invite_url}."
-puts 'Click on it to invite it to your server.'
+#probably a bad way to do this, but yey magic
+def command name
+  bot.message(content: "#{prefix+name}")
+  event.respond yield
+end 
 
 
 bot.message(contains: /\byoshiko\b/i) do |event|
-  event.respond 'Yohane!'
+  lines = [
+	'*It\'s Yo-ha-ne!*',
+	'Yohane!'
+  ]
+  event.respond lines.sample
 end
 
+
+# command 'invite_url' do
+#   bot.invite_url
+# end
+bot.message(content: "#{prefix+'invite_url'}") do |event|
+  event.respond bot.invite_url
+end
 
 bot.run
