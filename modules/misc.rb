0e02b6d2037596dc0eb85631e21c5536f60808be
IEACATADB <eugenehope94@gmail.com>
2018-06-11 14:26:26 +0300

    ¯\_(ツ)_/¯

---
+++ modules/misc.rb
@@ -0,0 +1,12 @@
+module Bot
+    module DiscordCommands
+        module Misc
+            extend Discordrb::Commands::CommandContainer
+
+            command :shrug do 
+                '¯\_(ツ)_/¯'
+            end
+
+        end
+    end
+end
