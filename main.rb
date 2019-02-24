7cedb6f20b3bbeae15c9937e45b2622b3f937934
IEACATADB <IEACATADB@users.noreply.github.com>
2019-02-25 00:08:07 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -21,13 +21,6 @@ module Bot
   end
 
  
-  bot.message(contains: /(i guess)$/i) do |event|
-    lines = [
-    ':hagumiAngery:'
-    ]
-    event.respond lines.sample
-  end
- 
   bot.command :invite_url do
     bot.invite_url
   end
